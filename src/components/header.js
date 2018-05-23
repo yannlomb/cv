618247ab72711682bde66d84edc4051ccc6ddc2d
Yannick Lombart <yannicklombart@SAX-MAC-002-YLombart.local>
2018-05-23 22:24:23 +0200

    logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from './yannicklogo.svg'
 
 const Header = ({ siteTitle }) => (
   <div>
     <header className="green">
-      <img className="logo-start" src="/static/yannicklogo.svg" />
+      <img className="logo-start" src={logo} />
       <h1>
         Curriculum Vitae <br />
         <span className="name">Yannick Lombart</span>
